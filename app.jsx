dd8fdd95350b80e602bfed1324f633847f3e6676
TheFrameForge <itssswooshydog@gmail.com>
2025-07-03 15:54:37 -0700

    s

---
+++ app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+    return (
+        <div>
+            <div className="my-element"></div>
+            <div className="text-1">THIS IS</div>
+            <div className="nav-box"></div>
+            <div className="text-our">OUR</div>
+            <div className="text-design">DESIGN</div>
+            <div className="sol-pill"></div>
+            <div className="Sol-pill-img"></div>
+        </div>
+    );
+}
